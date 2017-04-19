2481e7b7761b8ffdd9a2402261f18aed37232201
Damian Senn <senn@puzzle.ch>
2017-04-19 16:49:11 +0200

    Shell script dialog

--- src/ui/components/tmpy-client/component.ts
+++ src/ui/components/tmpy-client/component.ts
@@ -6,14 +6,35 @@ const JANITOR_INTERVAL = 5 * 1000;
 
 export default class TmpyClient extends Component {
   @tracked tmpyFiles: TmpyFile[] = [];
+  @tracked showScript: boolean = false;
+  @tracked script: string;
 
   private janitor: number;
+  private _script: Promise<string>;
 
   onUpload(tmpyFiles: TmpyFile[]) {
     this.tmpyFiles = [
       ...this.tmpyFiles,
       ...tmpyFiles
     ];
+  }
+
+  toggleScript() {
+    this.showScript = Boolean(this.script) && !this.showScript;
+
+    if (!this._script) {
+      this._script = fetch('/tmpy.sh')
+        .then(res => res.text());
+      this._script.then(() => this.showScript = true);
+    }
+
+    this._script.then(s => {
+      this.script = s;
+    });
+  }
+
+  stopPropagation(e: MouseEvent) {
+    e.stopPropagation();
   }
 
   didInsertElement() {

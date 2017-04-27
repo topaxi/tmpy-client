330972eddc6d65f0664f7f4d2113d4be88a3fb33
Damian Senn <damian.senn@topaxi.codes>
2017-04-27 18:26:34 +0200

    minify css (once pipeline pr is merged)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const Funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
+const cssnext = require('postcss-cssnext');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
@@ -14,6 +17,30 @@ module.exports = function(defaults) {
         resolve({ jsnext: true, module: true, main: true }),
         commonjs()
       ]
+    },
+    postcssOptions: {
+      compile: {
+        enabled: false
+      },
+      filter: {
+        enabled: true,
+        map: { inline: false },
+        plugins: [
+          {
+            module: cssnano
+          },
+          {
+            module: autoprefixer,
+            options: {
+              browsers: [
+                '> 1%',
+                'last 3 versions',
+                'Firefox ESR'
+              ]
+            }
+          }
+        ]
+      }
     }
   });
 
